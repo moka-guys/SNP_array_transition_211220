1826345440cfddd8297c97403770a66809547c23
Erin Peat <55829630+ErinPeat@users.noreply.github.com>
2022-02-17 10:09:13 +0000

    Updating code after code review

--- snp_array_processing_V1.0.R
+++ snp_array_processing_V1.0.R
@@ -2,7 +2,7 @@ library(tidyverse) # V1.3.1
 ########################
 # This script has been created to find the range, mean and other stats from the 
 # output of the snp_array_v2.0R script 
-# 1) Takes the an input of a csv file  
+# 1) Takes the input of a csv file  
 # 2) Pivots the data  
 # 3) Creates stats 
 # 4) Saves the output
@@ -14,9 +14,9 @@ means_df <- read.delim("df_means_211218.csv", sep = '\t' , header = TRUE)   # Im
 means_df_longer <- means_df %>% 
   pivot_longer(cols = c("chr1",  "chr2",  "chr3"  ,"chr4"  ,"chr5" , "chr6" , "chr7",  "chr8",
                         "chr9" , "chr10", "chr11", "chr12", "chr13", "chr14", "chr15", "chr16",
-                        "chr17" ,"chr18" ,"chr20", "chr21" ,"chr22", "chrX" , "chrY") , 
+                        "chr17" ,"chr18" , "chr19", "chr20", "chr21" ,"chr22", "chrX" , "chrY") , 
                names_to = "chromosome", values_to = "num_probes") %>% # Pivot data frame longer for easier calculations 
-  group_by(chromosome) %>% # Calculate per chromsome 
+  group_by(chromosome) %>% # Calculate per chromosome 
   mutate(lower_range = min(num_probes, na.rm = TRUE), # Get stats, ignoring NA values
          upper_range = max(num_probes, na.rm = TRUE),
          mean = signif(mean(num_probes, na.rm = TRUE),3),
@@ -25,7 +25,7 @@ means_df_longer <- means_df %>%
          less_than_three = sum(num_probes <= 3,  na.rm = TRUE),
          row_count = sum(!is.na(num_probes)),
          perc_less_than_three = signif((less_than_three/row_count)*100,3)) %>% 
-  select(-c(num_probes)) %>% # Remove uneeded columns 
+  select(-c(num_probes)) %>% # Remove unneeded columns 
   distinct() %>%  # Select one row per column 
   arrange(chromosome)
 
