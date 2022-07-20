547bb7f5fa25f6c91d7e5adeda9b48539cbcca21
Erin Peat <55829630+ErinPeat@users.noreply.github.com>
2022-07-20 16:20:12 +0100

    Update script after code review

--- dgv_gold_tidy.R
+++ dgv_gold_tidy.R
@@ -28,21 +28,19 @@ tidy_dgv_gold_df <- dgv_gold_df %>%
   separate(V9, col_names, sep = ";") %>% # Separate the final column into multiple columns
   pivot_longer(all_of(col_names_two_less), names_to = "name", values_to = "value") %>% # Flip df to easily manipulate data
   mutate(value_alone = str_remove(value, ".*=")) %>% # Remove string up to "="
-  select(-c(value)) %>% # Drop unnecessary columns 
+  select(-c(value)) %>% # Drop unnecessary column 
   pivot_wider(names_from = name, values_from = value_alone) %>% 
   mutate(variant_sub_type = str_remove(variant_sub_type, ".*=")) %>% 
-  distinct() %>% #  Remove duplicates
   rename(chrom = V1, # rename 
-         chromStart = outer_start, # Array team request to have both 
+         chromStart = outer_start, # Array team have requested to have both 
          chromEnd = outer_end, 
          thickStart = inner_start,
-         thickEnd = inner_end
-         ) %>% 
+         thickEnd = inner_end) %>% 
   mutate(calc = str_c(num_samples, Number_of_unique_samples_tested, sep = "/"), # Make new columns for UCSC, some are required in bed file format
          frequency_round = round(((as.integer(num_samples)/as.integer(Number_of_unique_samples_tested))*100), digits = 1),
          Frequency = str_c(frequency_round, "%"),
          ID = str_c(Frequency, "(",calc, ")"),
-         ID_2 = ID, 
+         ID_2 = ID, # Variable duplicated for UCSC requirements for bedDetail format
          strand = ".",
          score = 0,  
 details_string = str_c("<br /><h3>  Frequency ", Frequency, " (", num_samples,  " out of ", Number_of_unique_samples_tested,
@@ -51,7 +49,7 @@ details_string = str_c("<br /><h3>  Frequency ", Frequency, " (", num_samples,  
 itemRgb = case_when(variant_sub_type == "Gain" ~ "47,63,235", # Blue for gain
                     variant_sub_type == "Loss" ~ "255,0,0")) %>% # Reds for loss
   select(chrom, chromStart, chromEnd, ID, score, strand, thickStart, thickEnd, itemRgb, ID_2, details_string) %>% # Select required columns
-  distinct() # Remove duplicates
+  distinct() # Some variants have inner and outer coordinates from the raw data, leading to duplicated entries & this distinct removes the duplicates 
 
 
 # One track needed per variant type 
@@ -66,3 +64,6 @@ tidy_dgv_gold_loss <- tidy_dgv_gold_df %>%
 write.table(tidy_dgv_gold_gain, "ucsc_hg38_DGVgold_gain_2016-05-15.bed", sep = '\t', row.names = FALSE, col.names = FALSE, quote = FALSE)
 
 write.table(tidy_dgv_gold_loss, "ucsc_hg38_DGVgold_loss_2016-05-15.bed", sep = '\t', row.names = FALSE, col.names = FALSE, quote = FALSE)
+
+
+
